6bfdcf6c3c12cd53ed9656180045d164eb3a132e
jameslunatic <jjluna@gbox.adnu.edu.ph>
2023-07-30 01:17:33 +0800

    Initial sharedpref

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,12 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:digihydro/mainpages/dashboard.dart';
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import 'index_screen.dart';
 import 'load_screen.dart';
-//import 'doctors_appointment.dart';
-//import 'privacy_screen.dart';
+
+//String? finalEmail;
 
 Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
   print('Handling a background message ${message.messageId}');
@@ -14,11 +16,21 @@ Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
   await FirebaseMessaging.instance.getInitialMessage();
+  SharedPreferences prefs = await SharedPreferences.getInstance();
+  var email = prefs.getString('email');
   FirebaseMessaging.onBackgroundMessage(_firebaseMessagingBackgroundHandler);
-  runApp(digihydro());
+  //runApp(digihydro());
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    theme: ThemeData(
+      primarySwatch: Colors.green,
+    ),
+    home: IndexScreen(),
+    //home: email == null ? IndexScreen() : dashBoard(),
+  ));
 }
 
-class digihydro extends StatefulWidget {
+/*class digihydro extends StatefulWidget {
   @override
   loginScreen createState() => loginScreen();
 }
@@ -31,7 +43,8 @@ class loginScreen extends State<digihydro> {
       theme: ThemeData(
         primarySwatch: Colors.green,
       ),
+      //home: email
       home: IndexScreen(),
     );
   }
-}
+}*/
